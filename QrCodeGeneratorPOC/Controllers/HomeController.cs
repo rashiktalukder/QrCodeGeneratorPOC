b63fd44ca3d0b7cf2f5a7708f1bb4529d4a21589
Kawsar Rashik <kawsar.rashik@bizzntek.com>
2024-10-30 01:32:14 +0600

    Add project files.

---
+++ QrCodeGeneratorPOC/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Mvc;
+using QrCodeGeneratorPOC.Models;
+using QRCoder;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.Drawing;
+using static QRCoder.PayloadGenerator;
+
+namespace QrCodeGeneratorPOC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new QrCodeModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(QrCodeModel qrCodeModel)
+        {
+            string modelInfo = $"ProductName: {qrCodeModel.ProductName}" +
+                $"\n\nProductType: {qrCodeModel.ProductType}" +
+                $"\n\nProductDescription: {qrCodeModel.ProductDescription}";
+
+            QRCodeGenerator qrCodeGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrCodeGenerator.CreateQrCode(modelInfo, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            var qrCodeAsBitmap = qrCode.GetGraphic(20);
+
+            qrCodeModel.QrImageUrl = $"data:image/png;base64,{Convert.ToBase64String(BitmapToByteArray(qrCodeAsBitmap))}";
+
+            return View(qrCodeModel);
+        }
+
+        private byte[] BitmapToByteArray(Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ QrCodeGeneratorPOC/Models/QrCodeModel.cs
@@ -0,0 +1,10 @@
+﻿namespace QrCodeGeneratorPOC.Models
+{
+    public class QrCodeModel
+    {
+        public string? ProductName {  get; set; }
+        public string? ProductType {  get; set; }
+        public string? ProductDescription {  get; set; }
+        public string? QrImageUrl {  get; set; }
+    }
+}
